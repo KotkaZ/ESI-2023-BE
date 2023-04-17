913472baaa0e94f88ff7270839c35282615a6d1d
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-04-18 00:04:00 +0300

    Discovery service set-up

--- bookings/src/main/java/com/esi/bookings/BookingsApplication.java
+++ bookings/src/main/java/com/esi/bookings/BookingsApplication.java
@@ -2,8 +2,10 @@ package com.esi.bookings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient 
 public class BookingsApplication {
 
 	public static void main(String[] args) {
