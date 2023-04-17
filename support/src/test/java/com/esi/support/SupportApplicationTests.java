3076ccfa21ede610a3b91c8922a39abd00176357
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-04-18 00:24:08 +0300

    Support service project initialization

---
+++ support/src/test/java/com/esi/support/SupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esi.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
