de80d32ddf78d71b94d9d385112ad3e7db874875
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-04-18 00:34:13 +0300

    Checking service project initialization

---
+++ checking/src/test/java/com/esi/checking/CheckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esi.checking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
