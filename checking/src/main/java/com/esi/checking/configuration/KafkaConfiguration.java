6e24fe828ee10e138598b2275578354185b00de5
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-04-21 23:10:16 +0300

    Checking service Kafka configuration

---
+++ checking/src/main/java/com/esi/checking/configuration/KafkaConfiguration.java
@@ -0,0 +1,16 @@
+package com.esi.checking.configuration;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public NewTopic CheckingTopicCreation(){
+        return TopicBuilder.name("checkingTopic")
+                .build();
+    }
+}
