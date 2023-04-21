e55b15d7b2a2c0df17972a8c77eddf0a4dfe8ac8
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-04-22 00:33:16 +0300

    Booking service Kafka consumer topic registration

--- bookings/src/main/java/com/esi/bookings/configuration/KafkaConfiguration.java
+++ bookings/src/main/java/com/esi/bookings/configuration/KafkaConfiguration.java
@@ -10,7 +10,19 @@ public class KafkaConfiguration {
 
     @Bean
     public NewTopic BookingTopicCreation(){
-    return TopicBuilder.name("bookingTopic")
-    .build();
+        return TopicBuilder.name("bookingTopic")
+        .build();
+    }
+
+    @Bean
+    public NewTopic PaymentTopicCreation(){
+        return TopicBuilder.name("paymentTopic")
+                .build();
+    }
+
+    @Bean
+    public NewTopic CheckingTopicCreation(){
+        return TopicBuilder.name("checkingTopic")
+                .build();
     }
 }
