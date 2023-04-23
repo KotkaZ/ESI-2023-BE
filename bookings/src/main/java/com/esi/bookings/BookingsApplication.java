29437649627339546fc1d4c6ad1e9de1df54c335
Fillip Molodtsov <greatdrummer1@gmail.com>
2023-04-23 09:06:36 +0300

    Booking service: checking if room exists by doing an api call to room service

--- bookings/src/main/java/com/esi/bookings/BookingsApplication.java
+++ bookings/src/main/java/com/esi/bookings/BookingsApplication.java
@@ -3,10 +3,20 @@ package com.esi.bookings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient 
 public class BookingsApplication {
+
+	@Bean
+	// To discovery service by the name
+	@LoadBalanced
+	public WebClient.Builder getWebClientBuilder() {
+		return  WebClient.builder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingsApplication.class, args);
