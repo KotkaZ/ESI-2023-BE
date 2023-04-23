726ca1b668640bcedfee37372ebb61f1bba9b947
Fillip Molodtsov <greatdrummer1@gmail.com>
2023-04-23 19:03:26 +0300

    Booking service: set booking as booked

--- bookings/src/main/java/com/esi/bookings/service/EventService.java
+++ bookings/src/main/java/com/esi/bookings/service/EventService.java
@@ -1,5 +1,8 @@
 package com.esi.bookings.service;
 
+import com.esi.bookings.mapper.BookingsMapper;
+import com.esi.bookings.model.BookingStatus;
+import com.esi.bookings.repository.BookingsRepository;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -10,12 +13,19 @@ import com.esi.bookings.dto.CheckingEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.OffsetDateTime;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EventService {
 
+    private final BookingsMapper mapper;
+    private final BookingsRepository repo;
     private final KafkaTemplate<String, BookingEvent> kafkaTemplate;
     
     public void publishBooking(BookingEvent event){
@@ -25,11 +35,30 @@ public class EventService {
 
     @KafkaListener(topics = "paymentTopic", groupId = "bookingGroup" )
     public void processPayment(PaymentEvent event){
+        log.info("[KAFKA] Log message - received from payment topic: {} ", event.toString());
+        switch (event.getStatus()) {
+            case SUCCEEDED -> processSuccessPayment(event);
+            default -> log.info("[KAFKA] Log message - recieved from payment topic: {} ", event.toString());
+        }
+    }
+
+    private void processSuccessPayment(PaymentEvent event){
         log.info("[KAFKA] Log message - recieved from payment topic: {} ", event.toString());
+        var booking = repo.findById(event.getBookingId())
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Booking not found"));
+
+        booking.setStatus(BookingStatus.BOOKED);
+        booking.setPaymentId(event.getId());
+        booking.setModifiedAt(OffsetDateTime.now());
+
+        repo.saveAndFlush(booking);
+
+        var eventMsg = mapper.mapToEvent(booking);
+        publishBooking(eventMsg);
     }
 
     @KafkaListener(topics = "checkingTopic", groupId = "bookingGroup" )
     public void processChecking(CheckingEvent event){
-        log.info("[KAFKA] Log message - recieved from checking topic: {} ", event.toString());
+        log.info("[KAFKA] Log message - received from checking topic: {} ", event.toString());
     }
 }
