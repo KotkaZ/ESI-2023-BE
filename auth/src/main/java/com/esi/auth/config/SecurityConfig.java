da33ce4d9c731d2d1be6d6750aae39cbebf8903b
Karl Rapur <rapurkarl@gmail.com>
2023-04-28 22:59:35 +0300

    Store secret key as application property

--- auth/src/main/java/com/esi/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/esi/auth/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.esi.auth.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import com.esi.auth.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,20 +14,13 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -47,50 +43,32 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return  http
-                .csrf(csrf -> csrf.disable())
-                .cors(withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/auth/signup", "/api/auth/authenticate", "/api/auth/login").permitAll()
-                        .anyRequest().authenticated())
-                // Enabling the session manager to control the session
-                .sessionManagement()
-                // .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-                // .STATELESS prevents the request from being saved in the session.
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                // Defining our authenticationProvider
-                //.authenticationProvider(authenticationProvider())
-                // We are telling Spring Boot to check authFilter first, then, check the UsernamePasswordAuthenticationFilter
-                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-                //.formLogin().disable()
-                .build();
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(withDefaults())
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/auth/signup", "/api/auth/authenticate", "/api/auth/login")
+                .permitAll()
+                .anyRequest().authenticated())
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+    public AuthenticationProvider authenticationProvider() {
+        val authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+        throws Exception {
         return config.getAuthenticationManager();
     }
 
-    // You need this to deactivate the cors issue
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080/", "http://localhost:9090/")); //or add * to allow all origins
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); //to set allowed http methods
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.setExposedHeaders(Arrays.asList("custom-header1", "custom-header2"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
--- auth/src/main/java/com/esi/auth/jwt/JwtService.java
+++ auth/src/main/java/com/esi/auth/jwt/JwtService.java
@@ -7,77 +7,57 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-
 import java.security.Key;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtService {
 
-    @Autowired
-    private MyUserDetailsService userDetailsService;
+    @Value("${esi.auth.secret}")
+    private String SECRET;
 
-    // A funtion to generate the token
-    // jwt has three componenets, all of them are called claims
-    // We need to store these three compoenents in a format that allows us to deal with them properly, and that is why we are using HashMap()
-    // A HashMap allows storing items in "key/value" pairs, and you can access them by an index of another type (e.g. a String).
-    // After that, we are passing the claims (HashMap()) and the user name to another function to create the jwt token.
-    public String generateToken(String userName){
-        Map<String,Object> claims = new HashMap<>();
+    private final MyUserDetailsService userDetailsService;
+
+    public String generateToken(String userName) {
+        Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
     }
 
 
     private String createToken(Map<String, Object> claims, String userName) {
-        // This is not required as  user authorities are verified in SecurityConfig/securityFilterChain()
-        // I am adding the user authorities/roles to the claims of the jwt token
-        // Just an example for some of you that might need to add some user data (e.g., isEmployee, isAdmin) to the claims of the jwt token
-        UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
-        Collection<? extends GrantedAuthority> roles = userDetails.getAuthorities();
+        val userDetails = userDetailsService.loadUserByUsername(userName);
+        val roles = userDetails.getAuthorities();
         String rolesClaim = roles.toString();
         log.info("rolesClaim  {} ", rolesClaim);
+
         int start = rolesClaim.indexOf("[");
         int end = rolesClaim.indexOf("]");
         rolesClaim = rolesClaim.substring(start + 1, end);
         log.info("claims  {} ", rolesClaim);
 
-        // After, extracting the authorities/roles and removing the "[" "]", e are adding the authorities/roles to the claim
         claims.put("roles", rolesClaim);
 
-        return Jwts.builder() //Adds all given name/value pairs to the JSON Claims in the payload.
-                .setClaims(claims) // setting our claims
-                // JWTs claims are pieces of information asserted about a subject,
-                // The subject identifies the principal that is the subject of the JWT.
-                // The subject of our JWT is the user in our case, and here we are using her/his name
-                .setSubject(userName)
-                //.addClaims(roles)
-                .setIssuedAt(new Date(System.currentTimeMillis())) // not required
-                // token for 2 months
-                .setExpiration(new Date(System.currentTimeMillis()+ 1000L*60*60*1440)) // when setting an expiration "Date", we use milliseconds, the easiet way to set it is using the following format 1000(1000 milliseconds  = second)*60 (1 second * 60 =  a minutes) *60 (1 minute * 60 =  1 hour) * 1440 (hours in 2 months) -> 1000*60*60 = 1 hour
-                // .signWith(Key key, SignatureAlgorithm alg) signs the constructed JWT with the specified key using the specified algorithm, producing a JWS.
-                // For the key, we are using a function to created, and for the SignatureAlgorithm we are using the "HS256" hashing algorithm.
-                // .compact() is the final step, and it builds the JWT and serializes it to a compact, URL-safe string according to the JWT Compact Serialization rules.
-                .signWith(signingKey(), SignatureAlgorithm.HS256).compact();
+        return Jwts.builder()
+            .setClaims(claims)
+            .setSubject(userName)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 1440))
+            .signWith(signingKey(), SignatureAlgorithm.HS256).compact();
     }
 
-    // For generating a secret key, You can use https://www.allkeysgenerator.com/Random/Security-Encryption-Key-Generator.aspx
-    // Check the HEX and it is better to use a 256-bit to generate your secret key
-    public static final String SECRET = "4D6351665468576D5A7134743777217A25432A462D4A614E645267556B586E32";
 
     private Key signingKey() {
-        // Creating a byte array for our key by decoding our SECRET using BASE64 encoding scheme
         byte[] keyDecoder = Decoders.BASE64.decode(SECRET);
-        // Keys.hmacShaKeyFor(bytes) converts the byte array into a valid Key using the HMAC-SHA algorithms based on the specified key byte array
         return Keys.hmacShaKeyFor(keyDecoder);
     }
 
@@ -88,41 +68,22 @@ public class JwtService {
 
     private Claims extractAllClaims(String token) {
         return Jwts
-                .parserBuilder()
-                .setSigningKey(signingKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+            .parserBuilder()
+            .setSigningKey(signingKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 
-    // A function to extract the subject of the token (user name)
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    // A function to extract the user roles/authorities
-    public String extractRoles(String token) {
-        String claimRoles = extractAllClaims(token).get("roles", String.class);
-        return  claimRoles;
-    }
-
-    // A function to extract the token expiration date
-    public Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-    // Not required anymore
-    /*  private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    } */
-
     public boolean validateToken(String token) {
         try {
-            // verify the integrity of the JWT token
             Jwts.parserBuilder().setSigningKey(signingKey()).build().parseClaimsJws(token);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
-            // JwtException can be thrown if the token is invalid or has expired
-            // IllegalArgumentException can be thrown if the token is empty or null
             return false;
         }
     }
