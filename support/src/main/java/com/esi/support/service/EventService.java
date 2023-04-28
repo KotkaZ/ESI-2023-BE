0492a1cc34bf21d57f53e25476367a4301b00eca
Jan-Erik Kalmus <janerik.kalmus98@gmail.com>
2023-04-28 23:21:00 +0300

    Create cleaning entity upon check-out

--- support/src/main/java/com/esi/support/service/EventService.java
+++ support/src/main/java/com/esi/support/service/EventService.java
@@ -1,6 +1,7 @@
 package com.esi.support.service;
 
-import org.springframework.kafka.core.KafkaTemplate;
+import com.esi.support.model.Support;
+import com.esi.support.repository.SupportRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +10,28 @@ import com.esi.support.dto.CheckingEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.OffsetDateTime;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EventService {
 
+    private final SupportRepository repository;
+
     @KafkaListener(topics = "checkingTopic", groupId = "supportGroup")
     public void processChecking(CheckingEvent event){
         log.info("[KAFKA] Log message - recieved from checking topic: {} ", event.toString());
+
+        if (event.getCheckOutAt() != null) {
+            var cleaningItem = Support
+                    .builder()
+                    .bookingId(event.getBookingId())
+                    .roomId(event.getRoomId())
+                    .requestedAt(OffsetDateTime.now())
+                    .build();
+
+            repository.saveAndFlush(cleaningItem);
+        }
     }
 }
